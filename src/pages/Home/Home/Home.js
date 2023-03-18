0df516da076a2adadb17a4844f19f3dc2ef9be5d
RHN007 <rakibul.nayon@outlook.com>
2023-03-18 12:42:08 +0600

    adding sidebar and posts

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Posts from '../Posts/Posts';
+
+import Sidebar from '../SideBar/SideBar';
+import './Home.css'
+
+
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <div className='home'>
+        <Posts/>
+        <Sidebar/>
+
+
+
+            </div>
         </div>
     );
 };
